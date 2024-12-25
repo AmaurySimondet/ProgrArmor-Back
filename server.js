bc427e13d6d35acc46f93042d303c0dd766f7c26
Amaury Simondet <amaury.simondet@hotmail.com>
2024-12-25 19:10:52 +0100

    here we go again

--- server.js
+++ server.js
@@ -49,8 +49,11 @@ app.use(session({
 }));
 
 //Body Parser
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(express.raw({ limit: '50mb' }));
 
 //PASSPORT
 app.use(passport.initialize());
